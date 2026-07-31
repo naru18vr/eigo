509cdd63c10eb28ae2254186765f4d3848be07cc
naru18 <nar812u@gmail.com>
2026-08-01 01:24:39 +0900

    Finish grammar selection learning states

--- data/eiken4LearningKeys.ts
+++ data/eiken4LearningKeys.ts
@@ -3,3 +3,4 @@ export const EIKEN4_GRAMMAR_PRACTICE_STATS_KEY = 'eiken4GrammarPracticeStatsV1';
 export const EIKEN4_GRAMMAR_PRACTICE_HISTORY_KEY = 'eiken4GrammarPracticeHistoryV1';
 export const EIKEN4_STEP_LEARNING_KEY = 'eiken4StepLearningV1';
 export const EIKEN4_MIXED_REVIEW_KEY = 'eiken4MixedReviewV1';
+export const EIKEN4_REVIEW_SCHEDULE_KEY = 'eiken4ReviewScheduleV1';
