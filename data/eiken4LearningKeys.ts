60b8864337c1af2e10a70bfa628b01ee3e421ead
naru18 <nar812u@gmail.com>
2026-08-02 00:41:11 +0900

    Track Try-it video progress key

--- data/eiken4LearningKeys.ts
+++ data/eiken4LearningKeys.ts
@@ -5,3 +5,4 @@ export const EIKEN4_STEP_LEARNING_KEY = 'eiken4StepLearningV1';
 export const EIKEN4_MIXED_REVIEW_KEY = 'eiken4MixedReviewV1';
 export const EIKEN4_REVIEW_SCHEDULE_KEY = 'eiken4ReviewScheduleV1';
 export const EIKEN4_GRAMMAR_GUIDE_PROGRESS_KEY = 'eiken4GrammarGuideProgressV1';
+export const EIKEN4_GRAMMAR_VIDEO_PROGRESS_KEY = 'eiken4GrammarVideoProgressV1';
